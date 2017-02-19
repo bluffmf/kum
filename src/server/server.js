cd7bdab8f866ae794ce27f3e0e2b6f3191de7aa0
bluffmf <bluffmf@gmail.com>
2017-02-19 13:05:49 +0200

    port

--- src/server/server.js
+++ src/server/server.js
@@ -23,6 +23,6 @@ app.get('/', function(req, res) {
 });
 
 
-app.listen(3000, function() {
+app.listen(process.env.PORT || 5000, function() {
     console.log('Express server listening on port 3000');
 });

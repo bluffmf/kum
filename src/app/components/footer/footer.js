c4c7b5ed4b23c31d1a3d4cd7cfdd3f78db0f6166
bluffmf <bluffmf@gmail.com>
2017-02-21 09:00:23 +0200

    fix css footer

--- src/app/components/footer/footer.js
+++ src/app/components/footer/footer.js
@@ -7,7 +7,7 @@ KumApp.controller('FooterCtrl', function ($scope) {
 
     vm.items = [
         { name: 'Телефон',      icon: 'phone',          icon_url: 'photo/svg/phone.svg' },
-        { name: 'Вконтакте',    icon: 'twitter',        icon_url: 'photo/svg/twitter.svg' },
+        { name: 'Вконтакте',    icon: 'vk',             icon_url: 'photo/svg/vk.svg' },
         { name: 'Google+',      icon: 'google_plus',    icon_url: 'photo/svg/google_plus.svg' }
     ];
 

6ba24113f7ee11fcb36cd61af4876337b0898ed3
bluffmf <bluffmf@gmail.com>
2017-02-24 08:15:48 +0200

    header and footer

--- src/app/components/container/container.js
+++ src/app/components/container/container.js
@@ -7,11 +7,6 @@ KumApp.controller('ContainerCtrl', function ($scope) {
         name: "Apple"
     };
 
-    // vm.items = [
-    //     { name: 'Phone', icon: 'phone', icon_url: 'svg/phone.svg' },
-    //     { name: 'Twitter', icon: 'twitter', icon_url: 'svg/twitter.svg' },
-    //     { name: 'Google+', icon: 'google_plus', icon_url: 'svg/google_plus.svg' }
-    // ];
 
 
     var imagePath = 'img/list/60.jpeg';
@@ -29,70 +24,7 @@ KumApp.controller('ContainerCtrl', function ($scope) {
             who: 'Min Li Chan',
             when: '3:08PM',
             notes: " I'll be in your neighborhood doing errands"
-        },
-        {
-            face : imagePath,
-            what: 'Brunch this weekend?',
-            who: 'Min Li Chan',
-            when: '3:08PM',
-            notes: " I'll be in your neighborhood doing errands"
-        },
-        {
-            face : imagePath,
-            what: 'Brunch this weekend?',
-            who: 'Min Li Chan',
-            when: '3:08PM',
-            notes: " I'll be in your neighborhood doing errands"
-        },
-        // {
-        //     face : imagePath,
-        //     what: 'Brunch this weekend?',
-        //     who: 'Min Li Chan',
-        //     when: '3:08PM',
-        //     notes: " I'll be in your neighborhood doing errands"
-        // },
-        // {
-        //     face : imagePath,
-        //     what: 'Brunch this weekend?',
-        //     who: 'Min Li Chan',
-        //     when: '3:08PM',
-        //     notes: " I'll be in your neighborhood doing errands"
-        // },
-        // {
-        //     face : imagePath,
-        //     what: 'Brunch this weekend?',
-        //     who: 'Min Li Chan',
-        //     when: '3:08PM',
-        //     notes: " I'll be in your neighborhood doing errands"
-        // },
-        // {
-        //     face : imagePath,
-        //     what: 'Brunch this weekend?',
-        //     who: 'Min Li Chan',
-        //     when: '3:08PM',
-        //     notes: " I'll be in your neighborhood doing errands"
-        // },
-        // {
-        //     face : imagePath,
-        //     what: 'Brunch this weekend?',
-        //     who: 'Min Li Chan',
-        //     when: '3:08PM',
-        //     notes: " I'll be in your neighborhood doing errands"
-        // },
-        // {
-        //     face : imagePath,
-        //     what: 'Brunch this weekend?',
-        //     who: 'Min Li Chan',
-        //     when: '3:08PM',
-        //     notes: " I'll be in your neighborhood doing errands"
-        // },
-        // {
-        //     face : imagePath,
-        //     what: 'Brunch this weekend?',
-        //     who: 'Min Li Chan',
-        //     when: '3:08PM',
-        //     notes: " I'll be in your neighborhood doing errands"
-        // },
+        }
     ];
 
 });
